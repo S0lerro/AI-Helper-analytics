48e91bf28c9afc3c2d1724c6b9814cc082b74303
Andrew <belyaev.andrey.smr@gmail.com>
2024-12-09 18:09:52 +0400

    debug

--- bot.py
+++ bot.py
@@ -8,9 +8,11 @@ TOKEN = t.read()
 t.close()
 print(TOKEN)
 bot = telebot.TeleBot(TOKEN)
-users = ['']
+users = [1716995834]
 
 def add_to_db(ID):
+    users.append(ID)
+    print(users)
     pass
 
 
@@ -21,16 +23,16 @@ def daily_send_message():
 def schedule_daily_poll():
     scheduler = BackgroundScheduler()
 
-    scheduler.add_job(daily_send_message(), 'cron', hour=7, minute=0)
-    scheduler.add_job(daily_send_message(), 'cron', hour=13, minute=0)
-    scheduler.add_job(daily_send_message(), 'cron', hour=19, minute=0)
+    scheduler.add_job(daily_send_message, 'cron', hour=7, minute=0)
+    scheduler.add_job(daily_send_message, 'cron', hour=13, minute=0)
+    scheduler.add_job(daily_send_message, 'cron', hour=18, minute=0)
 
     scheduler.start()
 
-@bot.message_handler(commands="start")
+@bot.message_handler(commands=["start"])
 def start(message):
-    bot.send_message(message.chat_id, "Добро пожаловать бла бла бла")
-    add_to_db(message.chat_id)
+    bot.send_message(message.chat.id, "Добро пожаловать бла бла бла")
+    add_to_db(message.chat.id)
 
 if __name__ == '__main__':
     schedule_daily_poll()
