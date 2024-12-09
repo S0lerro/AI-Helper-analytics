7e3f52ad2cee061fabad777981ff315f7c27e8dd
Andrew <belyaev.andrey.smr@gmail.com>
2024-12-09 16:46:25 +0400

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,35 @@
+import telebot
+import schedule
+import time
+
+
+TOKEN = '8051592264:AAGYDcT8cLkJ0hiJTxHJsv4VBiQfy39zQy4'
+
+bot = telebot.TeleBot(TOKEN)
+users = ['']
+
+def add_to_db(ID):
+    pass
+
+
+def daily_send_message():
+    for chat_id in users:
+        bot.send_message(chat_id, 'текст')
+
+def schedule_daily_poll():
+    scheduler = schedule.BackgroundScheduler()
+
+    scheduler.add_job(daily_send_message(), 'cron', hour=7, minute=0)
+    scheduler.add_job(daily_send_message(), 'cron', hour=13, minute=0)
+    scheduler.add_job(daily_send_message(), 'cron', hour=19, minute=0)
+
+    scheduler.start()
+
+@bot.message_handler(commands="start")
+def start(message):
+    bot.send_message(message.chat_id, "Добро пожаловать бла бла бла")
+    add_to_db(message.chat_id)
+
+if __name__ == '__main__':
+    schedule_daily_poll()
+    bot.polling(none_stop=True)
